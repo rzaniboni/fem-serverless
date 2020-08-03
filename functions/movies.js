9d4e274c4e3f1cce5d4425e26c10e5596116d128
Roberto Zaniboni <zaniboni@gmail.com>
2020-08-03 08:28:32 +0200

    Add scores and actors to movie

--- functions/movies.js
+++ functions/movies.js
@@ -1,9 +1,32 @@
+const { URL } = require('url')
+const fetch = require('node-fetch')
+
 const movies = require('../data/movies.json')
 
 exports.handler = async () => {
+  const api = new URL('https://www.omdbapi.com/')
+
+  api.searchParams.set('apiKey', process.env.OMDB_API_KEY)
+
+  const promises = movies.map(movie => {
+    // use the movie's IMDb ID to lookup details
+    api.searchParams.set('i', movie.id)
+
+    return fetch(api).then(response => response.json()).then(data => {
+      const scores = data.Ratings;
+      console.log(data)
+      return {
+        ...movie,
+        scores,
+        actors: data.Actors
+      }
+    })
+  })
+
+  const moviesWithRatings = await Promise.all(promises)
 
   return {
     statusCode: 200,
-    body: JSON.stringify(movies)
+    body: JSON.stringify(moviesWithRatings)
   }
 }
